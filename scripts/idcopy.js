ab42790ccc83e26b002cd9bf571a5cd6ad10c0a4
Gratia <yhs.li957@gmail.com>
2026-07-05 15:35:14 +0800

    chore: remove legacy static site leftovers

--- scripts/idcopy.js
+++
@@ -1,9 +0,0 @@
-function copyLineID() {
-  const text = "@lua6791g";
-  navigator.clipboard.writeText(text).then(() => {
-    alert("LINE ID 已複製！");
-  }).catch(err => {
-    alert("複製失敗，請手動複製。");
-    console.error(err);
-  });
-}
